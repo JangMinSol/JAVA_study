741595f87f0e74cd0971106c1084ce7cab262316
sol03 <sol03@LAPTOP-LBH8PG2Q>
2022-01-06 00:02:19 +0900

    JAVA_study - upload(Data(Variable))

---
+++ Data/Variable.java
@@ -0,0 +1,22 @@
+public class Variable{
+    public static void main(String[] args) {
+    	int a;
+    	a = 1;
+    	System.out.println(a+1); //2
+    	System.out.println(a+1.5); //2.5
+    	
+    	double b;
+    	b = 5.1;
+    	System.out.println(b+1); //6.1
+    	
+    	//String course;
+    	//course = "Java";
+    	String course = "Java";
+    	System.out.println(course);
+    	
+    	String c, d;
+    	c = "Python";
+    	d = "Coding";
+    	System.out.println(c+d); //PythonCoding
+    }
+}
